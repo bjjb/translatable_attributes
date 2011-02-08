2adae515b3065f9040c8a0e59cb1aa8c16741cce
Bryan JJ Buckley <jj@bjjb.org>
2011-02-08 17:01:44 +0100

    Updated docs and tests for new version
    v0.0.3 will hook into ActiveModel::AttributeMethods instead - should be more
    robust, and it's already been written.

--- lib/translatable_attributes.rb
+++ lib/translatable_attributes.rb
@@ -4,8 +4,7 @@ require 'i18n/active_record'
 # attributes whose values are persisted in multiple languages in the database.
 #
 # For example, supposing you have an online shop, and you need to store the
-# description of products in English and German. First, you should ensure that
-# I18n.available_locales returns [:en, :de] (at least).
+# description of products in English (en) and Swiss-German (de_ch)
 #
 #     class Product
 #       include TranslatableAttributes
@@ -14,16 +13,15 @@ require 'i18n/active_record'
 #     
 #     product = Product.create                        => a new Product#123
 #     product.description_en = "A thing"
-#     product.description_de = "Ein Ding"
+#     product.description_de_ch = "Ein Ding"
 #
 # The descriptions will be persisted in the translations table (or whatever
-# your I18n::Backend::ActiveRecord is using - see that gem for more info),
-# with the key 'product.descriptions.123'.
+# your I18n::Backend::ActiveRecord is using - see that gem for more info).
 module TranslatableAttributes
+
   def self.included(mod)
     mod.send(:extend, ClassMethods)
     mod.send(:include, InstanceMethods)
-    mod.send(:after_save, :save_translatable_attributes)
   end
 
   module ClassMethods
@@ -39,6 +37,23 @@ module TranslatableAttributes
     #
     # This creates the methods `title_en`, `title_en=`, `title_de`,
     # `title_de=`, `body_en`, `body_en=`, `body_de` and `body_de=`
+    # 
+    # You can see the available translations for each attribute with
+    # `translated_titles`, and `translated_bodies`.
+    # 
+    # Like regular `attr_accessor` methods, you can call them separately:
+    #
+    #     class Post < ActiveModel::Base
+    #       translatable_attribute_accessor :title
+    #       translatable_attribute_accessor :body, :locales => [:all, :it]
+    #     end
+    #
+    # In the previous example, all Posts will additionally have a
+    # "translation" for the body in Italian (which is null, initially), even
+    # if `I18n.available_locales` doesn't include that locale.
+    #
+    # Regular attribute methods all apply, such as `body_en?` and
+    # `body_en_before_type_cast` (see ActiveModel::AttributeMethods).
     def translatable_attribute_accessor(*attributes)
       attributes.each do |attr|
         I18n::Backend::ActiveRecord::Translation.available_locales.map(&:to_s).each do |locale|
@@ -79,6 +94,20 @@ module TranslatableAttributes
       @translatable_attribute_records[locale] ||= {}
       @translatable_attribute_records[locale][attr] ||= I18n::Backend::ActiveRecord::Translation.locale(locale).find_by_key(key)
       @translatable_attribute_records[locale][attr] ||= I18n::Backend::ActiveRecord::Translation.locale(locale).build
+    end
+  end
+
+  class << self
+    def available_locales
+      I18n.available_locales
+    end
+
+    def locales
+      available_locales.map(&:to_s)
+    end
+
+    def locale_suffices
+      locales.map { |l| l.gsub('-', '_') }
     end
   end
 end
--- test/translatable_attributes_test.rb
+++ test/translatable_attributes_test.rb
@@ -2,48 +2,77 @@ $: << File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'test/unit'
 require 'translatable_attributes'
 
+# Prepare database
+ActiveRecord::Base.establish_connection(
+  :adapter => 'sqlite3',
+  :database => ':memory:'
+)
+# Create Translations table
+ActiveRecord::Base.connection.create_table :translations, :force => true do |t|
+  t.string :locale
+  t.string :key
+  t.string :value
+  t.text :interpolations
+  t.boolean :is_proc
+  t.references :translatable, :polymorphic => true
+end
+# Set the I18n backend
+I18n.backend = I18n::Backend::ActiveRecord.new
+# Create Models table
+ActiveRecord::Base.connection.create_table :models, :force => true do |t|
+  t.string :name
+end
+
 class TranslatableAttributesTest < Test::Unit::TestCase
-  class Model < ActiveRecord::Base; end
+# Create the test model
+  class Model < ActiveRecord::Base
+    include TranslatableAttributes
+  end
 
   def setup
-    ActiveRecord::Base.establish_connection(
-      :adapter => 'sqlite3',
-      :database => ':memory:'
-    )
-
-    ActiveRecord::Base.connection.create_table :models, :force => true do |t|
-      t.string :name
-    end
-
-    ActiveRecord::Base.connection.create_table :translations, :force => true do |t|
-      t.string :locale
-      t.string :key
-      t.string :value
-      t.text :interpolations
-      t.boolean :is_proc
-    end
-
-    I18n.backend = I18n::Backend::ActiveRecord.new
-    I18n::Backend::ActiveRecord::Translation.create(:key => 'x', :value => 'X', :locale => :en)
-    I18n::Backend::ActiveRecord::Translation.create(:key => 'x', :value => 'X', :locale => :de_de)
-
-    Model.send(:include, TranslatableAttributes)
-    Model.send(:translatable_attribute_accessor, :foo)
-
+    I18n.translate("English", :locale => :en)
     @model = Model.create!
   end
 
+  def test_gets_locales_from_i18n
+    assert_equal I18n.available_locales, TranslatableAttributes.available_locales
+    assert_equal I18n.available_locales.map(&:to_s), TranslatableAttributes.locales
+    I18n.available_locales = %W(xx yy-ZZ)
+    assert_equal [:xx, :"yy-ZZ"], I18n.available_locales
+    assert_equal [:xx, :"yy-ZZ"], TranslatableAttributes.available_locales
+    assert_equal %W(xx yy-ZZ), TranslatableAttributes.locales
+    assert_equal %W(xx yy_ZZ), TranslatableAttributes.locale_suffices
+  end
+
   def test_setup
-    assert_nothing_raised do
+    assert_nothing_raised "something's wrong with ActiveRecord?" do
       model = Model.create!(:name => "Heidi Klum")
       assert_equal "Heidi Klum", model.name
     end
     assert_nothing_raised do
-      I18n::Backend::ActiveRecord::Translation.create!(:locale => 'en', :key => 'foo', :value => 'bar')
-      assert_equal "bar", I18n.t("foo")
+      Model.send(:translatable_attribute_accessor, :foo)
+      I18n::Backend::ActiveRecord::Translation.create!(:locale => :en, :key => 'foo', :value => 'bar')
+      assert_equal "bar", I18n.t("foo", :locale => :en), "something's wrong with I18n::Backend"
     end
   end
 
+  def test_sanity
+    
+  end
+
+  def test_works_for_existing_locales
+    I18n.available_locales = :en
+    Model.send(:translatable_attribute_accessor, :x)
+    model = Model.new
+    assert_respond_to model, :x
+    assert_respond_to model, :x=
+    assert_respond_to model, :x_en
+    assert_respond_to model, :x_en=
+    assert_respond_to model, :x_en?
+    assert_respond_to model, :x_before_type_cast
+  end
+
+if false
   def test_creation_of_methods
     assert @model.respond_to?('foo_en')
     assert @model.respond_to?('foo_en=')
@@ -77,4 +106,13 @@ class TranslatableAttributesTest < Test::Unit::TestCase
     key = @model.translatable_attribute_record_key("x_y")
     assert_equal "models.#{@model.id}.x_y", key
   end
+
+  def test_existing_records_are_not_needed
+    I18n::Backend::ActiveRecord::Translation.delete_all
+    m = Model.create(:foo_en => "Boo", :foo_de => "Bü", :foo_it => "Bú")
+    assert_equal "Boo", m.foo_en
+    assert_equal "Bü", m.foo_en
+    assert_equal "Bú", m.foo_en
+  end
+end
 end
